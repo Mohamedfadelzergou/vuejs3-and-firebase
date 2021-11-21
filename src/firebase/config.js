4164fdce3f748cd8dec7c247a4fca26f45e4239d
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-11-21 14:16:38 +0100

    Connecting Vue 3 with Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyBDmKm_WBAdap2fFIRNeVkBWCjCBIqwxFw",
+    authDomain: "vuejs-firebase-d24d6.firebaseapp.com",
+    projectId: "vuejs-firebase-d24d6",
+    storageBucket: "vuejs-firebase-d24d6.appspot.com",
+    messagingSenderId: "3632533542",
+    appId: "1:3632533542:web:fb911b07d5b57f5041d6e7"
+  };
+firebase.initializeApp(firebaseConfig);
+const projectFirestore = firebase.firestore();
+export {projectFirestore};
